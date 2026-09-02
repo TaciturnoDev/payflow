3bcc4939baf040703252d9e1ff829aba74bbd5d7
TaciturnoDev <mathdev.0369@gmail.com>
2026-09-02 17:13:02 -0300

    chore: initialize PayFlow project

---
+++ src/main/java/com/payflow/entity/Wallet.java
@@ -0,0 +1,26 @@
+package com.payflow.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class Wallet {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private BigDecimal balance;
+
+}
